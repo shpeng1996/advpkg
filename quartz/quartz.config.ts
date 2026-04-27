9ef20d1fc6bb1935cc15bdb100fdede2a67729f9
shenghsuanpeng <shenghsuanpeng@gmmcorp.com.tw>
2026-04-27 09:58:26 +0800

    Refactor: Simplify page title in Quartz configuration

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "先進封裝技術資料庫 / Advanced Packaging TechDB",
+    pageTitle: "AdvPkg",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
